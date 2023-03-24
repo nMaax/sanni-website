6fd48680ca9c6774bf9226a2484735bcc0385edf
nMaax <carli.massimiliano01@gmail.com>
2023-03-24 23:58:45 +0100

    Defined LICENCE file and made more clear README file, improved index and faq pages and added new stock photos to landing page

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,9 @@
 'use strict'
+/*!
+* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
 
 /** Cookie toast policy */
 window.addEventListener("load", function() {
